bd43a0a5d0b351a0876983a3ccd5b8d26facdfd2
akashc-hub <akashchauhan84590@gmail.com>
2026-09-24 14:23:40 +0530

    new commit

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -52,5 +52,12 @@ app.MapPut("/games/{id}", (int id, CreateGameDto updatedGame) =>
     return Results.NoContent();
 });
 
+//DELETE /games/1
+app.MapDelete("/games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
     
 app.Run();
